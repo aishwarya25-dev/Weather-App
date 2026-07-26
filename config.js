2f1849b0e38a0eeabc273f71b957fa6a1eaa7b71
aishwarya25-dev <aishwaryaj2510@gmail.com>
2026-07-26 18:17:20 +0530

    Added .gitignore

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// Create your own Weather api key from "Open Weather website and paise in YOUR_OPENWEATHER_API_KEY_HERE".
-
-const apiKey = "YOUR_OPENWEATHER_API_KEY_HERE";
+const apiKey = "b6fe3db60c6574dace589b0e922bb647";
